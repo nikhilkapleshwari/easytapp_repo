c68244ebfae192d7b15b30920caf49c27263394c
nikhil kapleshwari <nikhil1632@gmail.com>
2019-04-29 11:01:25 +0530

    cleaned

--- EasyTappApi/src/main/java/com/repository/CounterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.dao.Counter;
-
-public interface CounterRepository extends MongoRepository<Counter,String>{
-
-}
