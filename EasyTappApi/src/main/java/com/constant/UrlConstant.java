74746abcc3d5d9f0ccc060dde4989f7e00b8d54f
nikhil kapleshwari <nikhil1632@gmail.com>
2019-04-29 11:02:33 +0530

    again first version

---
+++ EasyTappApi/src/main/java/com/constant/UrlConstant.java
@@ -0,0 +1,12 @@
+package com.constant;
+
+public class UrlConstant {
+
+	private UrlConstant() {
+
+	}
+
+	public static final String VERIFY_ACCOUNT = "http://139.59.59.19:3001/verifyAccount";
+	public static final String SEND_FORGOT_PWD_MAIL = "http://139.59.59.19:3001/sendForgotPwdMail";
+
+}
